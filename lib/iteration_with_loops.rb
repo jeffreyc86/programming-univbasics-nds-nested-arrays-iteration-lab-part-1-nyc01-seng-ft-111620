8edb16126fd4c557e967d914592d0ae24add2613
Jeffrey Chiu <mr.jeffrey.chiu@gmail.com>
2020-11-01 22:19:19 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -7,7 +7,7 @@ def find_even_values(src)
     ia_count = 0 
     while ia_count < src[oa_count].size do
       if src[oa_count][ia_count] % 2 = 0
-      puts src[oa_count][ia_count]
+      p src[oa_count][ia_count]
       end
     ia_count += 1
     end
